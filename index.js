8bc6269556e261d6d8083ead5842030c9ffdb888
Mario Costa Junior <mario.junior.pro@gmail.com>
2017-12-14 17:10:33 -0200

    initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+const bodyParser = require("body-parser");
+require("./models/User");
+require("./services/passport");
+
+mongoose
+  .connect(keys.mongoURI)
+  .then(
+    () => console.log("Mongoose connected!"),
+    err => console.log("Error connecting Mongoose!", err)
+  );
+
+const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(
+  bodyParser.urlencoded({
+    // to support URL-encoded bodies
+    extended: true
+  })
+);
+
+require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
+
+if (process.env.NODE_ENV === "production") {
+  // Serve react client
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
---
+++ routes/billingRoutes.js
@@ -0,0 +1,21 @@
+const passport = require("passport");
+const keys = require("../config/keys");
+const stripe = require("stripe")(keys.stripeSecretKey);
+const requireLogin = require("../middlewares/requireLogin");
+
+module.exports = app => {
+  app.post("/api/stripe", requireLogin, async (req, res) => {
+    const token = req.body.id;
+
+    const charge = await stripe.charges.create({
+      amount: 5 * 100,
+      currency: "usd",
+      description: "$5 for 5 email credits",
+      source: token
+    });
+
+    req.user.credits += 5;
+    const user = await req.user.save();
+    res.send(user);
+  });
+};
